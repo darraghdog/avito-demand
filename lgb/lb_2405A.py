f1ba544d8d987dfc06bfcc8a1c4461af22d0828c
dhanley2 <darragh_hanley@optum.com>
2018-05-24 20:14:41 +0100

    lgb

--- lgb/lb_2405A.py
+++ lgb/lb_2405A.py
@@ -60,8 +60,11 @@ print('[{}] Load engineered features'.format(time.time() - start_time))
 featusrttl = pd.read_csv(path + '../features/user_agg.csv.gz', compression = 'gzip') # created with features/make/user_actagg_1705.py
 featusrcat = pd.read_csv(path + '../features/usercat_agg.csv.gz', compression = 'gzip') # created with features/make/user_actagg_1705.py
 featusrprd = pd.read_csv(path + '../features/user_activ_period_stats.gz', compression = 'gzip') # created with features/make/user_actagg_1705.py
-featrdgtxt = pd.read_csv(path + '../features/ridgeText5CV.csv.gz', compression = 'gzip') # created with features/make/user_actagg_1705.py
+#featrdgtxt = pd.read_csv(path + '../features/ridgeText5CV.csv.gz', compression = 'gzip') # created with features/make/user_actagg_1705.py
+featrdgtxts = pd.read_csv(path + '../features/ridgeTextStr5CV.csv.gz', compression = 'gzip') # created with features/make/user_actagg_1705.py
 featrdgimg = pd.read_csv(path + '../features/ridgeImg5CV.csv.gz', compression = 'gzip') # created with features/make/user_actagg_1705.py
+featrdgtxts.drop(['ridge_preds_strtxt1', 'ridge_preds_strtxt2'], 1, inplace = True)
+featrdgtxts.head()
 
 featenc = pd.read_csv(path + '../features/alldf_bayes_mean.gz', compression = 'gzip') # created with features/make/user_actagg_1705.py
 featct  = pd.read_csv(path + '../features/alldf_count.gz', compression = 'gzip') # created with features/make/user_actagg_1705.py
@@ -84,13 +87,16 @@ df.drop(['idx'], axis=1,inplace=True)
 df.reset_index(inplace = True)
 
 df.head()
-df = pd.concat([df.reset_index(),featenc, featct],axis=1)
-df['ridge_txt'] = featrdgtxt['ridge_preds'].values
+df = pd.concat([df.reset_index(),featenc, featct, featrdgtxts],axis=1)
+#df['ridge_txt'] = featrdgtxt['ridge_preds'].values
+#df = pd.concat([df.reset_index(),featenc, featct, ],axis=1)
+
 df['ridge_img'] = featrdgimg['ridge_img_preds'].values
 df = df.set_index('item_id')
 df.drop(['index'], axis=1,inplace=True)
 df.columns
 del featusrttl, featusrcat, featusrprd, featenc
+# del featusrttl, featusrcat, featusrprd, featenc, featrdgtxts
 gc.collect()
 
 print('[{}] Feature Engineering'.format(time.time() - start_time))
@@ -165,33 +171,33 @@ def parallelize(data, func):
     data = pd.concat([pd.Series(l) for l in pool.map(tokCol, data_split)]).values
     pool.close()
     pool.join()
-    return data  
+    return data
 
 load_text = True
-text_cols = ['description', 'text', 'text_feat']
+text_cols = ['description', 'text', 'text_feat', 'title']
 if load_text:
-    dftxt = pd.to_csv(path + '../features/text_features_morphed.csv.gz', compression = 'gzip')
+    dftxt = pd.read_csv(path + '../features/text_features_morphed.csv.gz', compression = 'gzip')
     for col in text_cols:
         print(col + ' load tokenised [{}]'.format(time.time() - start_time))
-        df[col] = dftxt[col]
+        df[col] = dftxt[col].values
+        df.fillna(' ', inplace = True)
+    del dftxt
 else:
     for col in text_cols:
         print(col + ' tokenise [{}]'.format(time.time() - start_time))
         df[col] = parallelize(df[col], tokCol)
     df[text_cols].to_csv(path + '../features/text_features_morphed.csv.gz', compression = 'gzip')
-    gc.collect()
-print('[{}] Finished tokenizing text...'.format(time.time() - start_time))
+gc.collect()
 
+print('[{}] Finished tokenizing text...'.format(time.time() - start_time))
+df.head()
 print('[{}] [TF-IDF] Term Frequency Inverse Document Frequency Stage'.format(time.time() - start_time))
 russian_stop = set(stopwords.words('russian'))
 tfidf_para = {
     "stop_words": russian_stop,
-    "analyzer": 'word',
     "token_pattern": r'\w{1,}',
     "sublinear_tf": True,
-    "lowercase": True,
     "dtype": np.float32,
-    "norm": 'l2',
     "smooth_idf":False
 }
 countv_para = {
@@ -214,10 +220,9 @@ vectorizer = FeatureUnion([
         ('title',CountVectorizer(
             **countv_para,
             preprocessor=get_col('title'))),
-        ('all_titles',TfidfVectorizer(
-            max_features=20000,
-            **tfidf_para,
-            preprocessor=get_col('all_titles')))
+        #('all_titles',CountVectorizer(
+        #    **countv_para,
+        #    preprocessor=get_col('title')))
     ])
     
 start_vect=time.time()
@@ -276,7 +281,7 @@ if full:
         valid_sets=[lgtrain, lgvalid],
         valid_names=['train','valid'],
         #early_stopping_rounds=500,
-        verbose_eval=200)    
+        verbose_eval=20)    
 else:
     lgb_clf = lgb.train(
         lgbm_params,
@@ -284,8 +289,8 @@ else:
         num_boost_round=15000,
         valid_sets=[lgtrain, lgvalid],
         valid_names=['train','valid'],
-        early_stopping_rounds=500,
-        verbose_eval=200)
+        early_stopping_rounds=30,
+        verbose_eval=20)
 
 # Feature Importance Plot
 f, ax = plt.subplots(figsize=[7,10])
@@ -298,43 +303,57 @@ print('RMSE:', np.sqrt(metrics.mean_squared_error(y_valid, lgb_clf.predict(X_val
 lgpred = lgb_clf.predict(testing)
 lgsub = pd.DataFrame(lgpred,columns=["deal_probability"],index=testdex)
 lgsub['deal_probability'].clip(0.0, 1.0, inplace=True) # Between 0 and 1
-lgsub.to_csv("../sub/lgsub_2305C.csv.gz",index=True,header=True, compression = 'gzip')
+lgsub.to_csv("../sub/lgsub_2405.csv.gz",index=True,header=True, compression = 'gzip')
 print("Model Runtime: %0.2f Minutes"%((time.time() - modelstart)/60))
 
 
 '''
-[200]   train's rmse: 0.211268  valid's rmse: 0.217224
-[400]   train's rmse: 0.203472  valid's rmse: 0.216138
-[600]   train's rmse: 0.198542  valid's rmse: 0.215859
-[800]   train's rmse: 0.194951  valid's rmse: 0.215775
-[1000]  train's rmse: 0.19212   valid's rmse: 0.21576
-[1200]  train's rmse: 0.189555  valid's rmse: 0.21577
-[1400]  train's rmse: 0.187162  valid's rmse: 0.215798
+[20]    train's rmse: 0.240958  valid's rmse: 0.23907
+[40]    train's rmse: 0.230399  valid's rmse: 0.229367
+[60]    train's rmse: 0.224392  valid's rmse: 0.224264
+[80]    train's rmse: 0.220644  valid's rmse: 0.22151
+[100]   train's rmse: 0.218087  valid's rmse: 0.219867
+[120]   train's rmse: 0.216137  valid's rmse: 0.218877
+[140]   train's rmse: 0.214506  valid's rmse: 0.218163
+[160]   train's rmse: 0.213069  valid's rmse: 0.217634
+[180]   train's rmse: 0.211874  valid's rmse: 0.217284
+[200]   train's rmse: 0.210779  valid's rmse: 0.217066
+[220]   train's rmse: 0.209689  valid's rmse: 0.216829
+[240]   train's rmse: 0.208705  valid's rmse: 0.216673
+[260]   train's rmse: 0.207825  valid's rmse: 0.21652
+[280]   train's rmse: 0.206934  valid's rmse: 0.216379
+[300]   train's rmse: 0.206133  valid's rmse: 0.216232
+[320]   train's rmse: 0.205342  valid's rmse: 0.216114
+[340]   train's rmse: 0.204563  valid's rmse: 0.215997
+[360]   train's rmse: 0.203892  valid's rmse: 0.215898
+[380]   train's rmse: 0.203276  valid's rmse: 0.215813
+[400]   train's rmse: 0.20259   valid's rmse: 0.215759
+[420]   train's rmse: 0.202005  valid's rmse: 0.2157
+[440]   train's rmse: 0.201416  valid's rmse: 0.215632
+[460]   train's rmse: 0.200846  valid's rmse: 0.215591
+[480]   train's rmse: 0.200275  valid's rmse: 0.215543
+[500]   train's rmse: 0.199783  valid's rmse: 0.215497
+[520]   train's rmse: 0.199257  valid's rmse: 0.215468
+[540]   train's rmse: 0.198798  valid's rmse: 0.215426
+[560]   train's rmse: 0.198328  valid's rmse: 0.215382
+[580]   train's rmse: 0.197896  valid's rmse: 0.215371
+[600]   train's rmse: 0.197433  valid's rmse: 0.215358
+[620]   train's rmse: 0.197013  valid's rmse: 0.215341
+[640]   train's rmse: 0.196605  valid's rmse: 0.215317
+[660]   train's rmse: 0.196176  valid's rmse: 0.215308
+[680]   train's rmse: 0.195761  valid's rmse: 0.215296
+[700]   train's rmse: 0.195366  valid's rmse: 0.215289
+[720]   train's rmse: 0.194985  valid's rmse: 0.215268
+[740]   train's rmse: 0.194613  valid's rmse: 0.215265
+[760]   train's rmse: 0.194261  valid's rmse: 0.215257
+[780]   train's rmse: 0.193927  valid's rmse: 0.215237
+[800]   train's rmse: 0.193568  valid's rmse: 0.215229
+[820]   train's rmse: 0.193249  valid's rmse: 0.215213
+[840]   train's rmse: 0.192924  valid's rmse: 0.215204
+[860]   train's rmse: 0.192558  valid's rmse: 0.215209
 Early stopping, best iteration is:
-[1001]  train's rmse: 0.192109  valid's rmse: 0.215757
+[838]   train's rmse: 0.192961  valid's rmse: 0.215201
 '''
 
-'''
-[200]   train's rmse: 0.224349  valid's rmse: 0.223056
-[400]   train's rmse: 0.220863  valid's rmse: 0.221124
-[600]   train's rmse: 0.218924  valid's rmse: 0.220071
-[800]   train's rmse: 0.217614  valid's rmse: 0.219446
-[1000]  train's rmse: 0.21655   valid's rmse: 0.218989
-[2000]  train's rmse: 0.21291   valid's rmse: 0.217881
-[3000]  train's rmse: 0.210391  valid's rmse: 0.21738
-[4000]  train's rmse: 0.208276  valid's rmse: 0.217065
-[5000]  train's rmse: 0.206422  valid's rmse: 0.216848
-[6000]  train's rmse: 0.204714  valid's rmse: 0.216689
-[7000]  train's rmse: 0.203123  valid's rmse: 0.216588
-[8000]  train's rmse: 0.201632  valid's rmse: 0.216497
-[9000]  train's rmse: 0.200206  valid's rmse: 0.216421
-[10000] train's rmse: 0.198872  valid's rmse: 0.216357
-[11000] train's rmse: 0.197587  valid's rmse: 0.21633
-[12000] train's rmse: 0.19633   valid's rmse: 0.216298
-[13000] train's rmse: 0.195128  valid's rmse: 0.216274
-[14000] train's rmse: 0.193947  valid's rmse: 0.216261
-[14800] train's rmse: 0.193038  valid's rmse: 0.216251
-[14380] train's rmse: 0.193493  valid's rmse: 0.216247
-'''
 
 
--- sub/blend/blend.R
+++ sub/blend/blend.R
@@ -10,12 +10,22 @@ library(Metrics)
 path = '~/avito/data/'
 path = '/Users/dhanley2/Documents/avito/data/'
 
-subbl = fread(paste0(path, '../sub/mlpblend_1905.csv'))
-subrn = fread(paste0(path, '../sub/rnnsub_2005.csv'))
-subbl = subbl[order(item_id)]
-subrn = subrn[order(item_id)]
+subbl = fread(paste0(path, '../sub/submission.csv'))[order(item_id)] # Public kernel -- https://www.kaggle.com/moussaid/moyen-just-simple-moyen-of-my-predictions/output
+sublg = fread(paste0(path, '../sub/lgsub_2405.csv'))[order(item_id)]
+subm1 = fread(paste0(path, '../sub/mlpsub_1905.csv'))[order(item_id)]
+subm2 = fread(paste0(path, '../sub/mlpsub_1705.csv'))[order(item_id)]
+subrn = fread(paste0(path, '../sub/rnnsub_2205.csv'))[order(item_id)]
 
-cor(subbl$deal_probability, subrn$deal_probability)
-subbld = subbl
-subbld$deal_probability = subbl$deal_probability*0.9 + subrn$deal_probability*0.1
-fwrite(subbld, paste0(path, '../sub/mlpblend_2105.csv'))
+cor(subbl$deal_probability, subm1$deal_probability)
+cor(subrn$deal_probability, subm1$deal_probability)
+cor(subm1$deal_probability, subm2$deal_probability)
+cor(sublg$deal_probability, subbl$deal_probability)
+
+mean()
+
+subbld  = copy(subbl)
+subbld$deal_probability = 0.2*(subbl$deal_probability + sublg$deal_probability) + 
+                            0.15*(subm1$deal_probability + subm2$deal_probability) + 
+                            0.3*(subrn$deal_probability)
+
+fwrite(subbld, paste0(path, '../sub/blendall_2405.csv'))
